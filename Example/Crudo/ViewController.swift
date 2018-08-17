2ef9d945b2b8605a5bbf310a221f50e8231d08cc
Jason Grandelli <jasongrandelli@gmail.com>
2018-08-17 13:33:44 -0400

    Build the initial project from pod init and tweak results

---
+++ Example/Crudo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Crudo
+//
+//  Created by Jason Grandelli on 08/16/2018.
+//  Copyright (c) 2018 Jason Grandelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
